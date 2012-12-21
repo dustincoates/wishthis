64c4d58bff23527ba58a33d30cad4948822d0102
Dustin Coates <dustin@dcoates.com>
2012-12-21 13:13:05 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wishthis::Application.config.secret_token = '1aa65be1009a633ff8dd922ef6c94c56cfe1023536357eadd7f187539f9a3355a61dfa07ab637120eb0a69750de3fd5bb3f53aa86fc828de11e097484b8f156d'
